e8c6430b4dc9862b04c8f6182e371b3fc0fb2836
KonstantinErlich <127948646+KonstantinErlich@users.noreply.github.com>
2024-09-03 10:23:55 +0200

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,9 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define SECRET_SSID "your-network-name"
+#define SECRET_PASS "your-network-password"
+#define SECRET_HUE_USERNAME "your-hue-user-name"
+#define SECRET_HUE_IP "your-hue-bridge-ip-adress"
+
+#endif
