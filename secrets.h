c0cb9bb7404c744a0a6c9b95a770b1ff50bdfdae
KonstantinErlich <127948646+KonstantinErlich@users.noreply.github.com>
2024-09-03 10:42:32 +0200

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -4,6 +4,6 @@
 #define SECRET_SSID "your-network-name"
 #define SECRET_PASS "your-network-password"
 #define SECRET_HUE_USERNAME "your-hue-user-name"
-#define SECRET_HUE_IP "your-hue-bridge-ip-adress"
+#define SECRET_HUE_IP your-hue-bridge-ip-adress
 
 #endif
